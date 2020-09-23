7af9bd7eb16b58c1381cefc7c0fe9408315ec193
joshphur <j4matt@gmail.com>
2020-09-23 10:52:43 +0100

    second commit

--- T508AC_DEMO_AS/app/src/main/java/com/POS/apis/FingerprintController/Dashboard.java
+++ T508AC_DEMO_AS/app/src/main/java/com/POS/apis/FingerprintController/Dashboard.java
@@ -1,15 +1,34 @@
 package com.POS.apis.FingerprintController;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import jepower.com.t508ac_demo.R;
 
-public class Dashboard extends AppCompatActivity {
+public class Dashboard extends Activity {
 
+    Button btnToGoBallot;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        btnToGoBallot = findViewById(R.id.btnToGoBallot);
+
+        btnToGoBallot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToBallot();
+            }
+        });
+    }
+
+    public void goToBallot() {
+        Intent intent = new Intent(this, PresidentialBallot.class);
+        startActivity(intent);
     }
 }
